53ee5e5f8cc849aba159d1d235ea798a2c45330a
marcialfps <marcialfco@live.com>
2023-05-15 19:22:14 +0100

    Add cors to API

--- api/app.js
+++ api/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 //Se obtienen todas las rutas de los ficheros de rutas y se asignan a su recurso correspondiente
 app.use("/bulk", require("./routes/bulk"));
